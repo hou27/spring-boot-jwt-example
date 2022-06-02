06a5352fb94f8ccdbc9d6615d59bbbe2dc48a098
hou27 <ataj125@gmail.com>
2022-06-02 22:38:50 +0900

    Remove old code

--- src/main/java/demo/api/config/SecurityConfig.java
+++ src/main/java/demo/api/config/SecurityConfig.java
@@ -30,11 +30,6 @@ public class SecurityConfig {
   }
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//    http
-//        .authorizeHttpRequests((authz) -> authz
-//            .anyRequest().authenticated()
-//        )
-//        .httpBasic(withDefaults());
     http
         .csrf().disable()
         .formLogin()
@@ -50,62 +45,4 @@ public class SecurityConfig {
         .anyRequest().authenticated();
     return http.build();
   }
-
 }
-
-
-
-///**
-// * Spring Security 사용을 위한 Configuration Class를 작성하기 위해서
-// * WebSecurityConfigurerAdapter를 상속하여 클래스를 생성하고
-// * @Configuration 애노테이션 대신 @EnableWebSecurity 애노테이션을 추가한다.
-// */
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//  private final UserDetailsService userDetailsService;
-//
-//  /**
-//   * PasswordEncoder를 Bean으로 등록
-//   */
-//  @Bean
-//  public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//    return new BCryptPasswordEncoder();
-//  }
-//
-////  /**
-////   * 인증 or 인가가 필요 없는 경로를 설정
-////   */
-////  @Override
-////  public void configure(WebSecurity web) throws Exception {
-////    web.ignoring().antMatchers("/?/**");
-////  }
-//
-//  /**
-//   * 인증에 대한 지원
-//   */
-//  @Override
-//  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-//  }
-//
-//  /**
-//   * 인증 or 인가에 대한 설정
-//   */
-//  @Override
-//  protected void configure(HttpSecurity http) throws Exception {
-//    http
-//        .csrf().disable()
-//        .formLogin()
-//        .loginPage("/user/signIn")
-//        .loginProcessingUrl("/user/signInProc")
-//        .usernameParameter("email")
-//        .passwordParameter("password")
-//        .defaultSuccessUrl("/")
-//        .failureUrl("/user/signIn?fail=true");
-//    http
-//        .authorizeRequests()
-//        .antMatchers("/", "/user/signUp", "/user/userList", "/user/signIn*").permitAll()
-//        .anyRequest().authenticated();
-//  }
-//}
