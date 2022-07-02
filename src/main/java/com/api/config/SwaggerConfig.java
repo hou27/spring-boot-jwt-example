f98d8199495f15b5de3cae9fba307175f5732304
hou27 <ataj125@gmail.com>
2022-07-02 22:46:47 +0900

    Add SecurityItem - swagger

--- src/main/java/com/api/config/SwaggerConfig.java
+++ src/main/java/com/api/config/SwaggerConfig.java
@@ -3,7 +3,10 @@ package com.api.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,10 +28,16 @@ public class SwaggerConfig {
         .build();
   }
 
+  // https://swagger.io/docs/specification/authentication/
   @Bean
   public OpenAPI customOpenAPI() {
+    SecurityScheme bearerAuth = new SecurityScheme()
+        .type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("Bearer");
+    SecurityRequirement securityItem = new SecurityRequirement().addList("bearerAuth");
+
     return new OpenAPI()
-        .components(new Components())
+        .components(new Components().addSecuritySchemes("bearerAuth", bearerAuth))
+        .addSecurityItem(securityItem)
         .info(new Info().title("Spring Boot API Example")
             .description("Spring Boot API 예시 프로젝트입니다.")
             .version("v0.0.1"));
